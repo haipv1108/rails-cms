64c0e1271ecc0b273922de102487bf50e5ac2af8
HaiLeader <pham.haisv@gmail.com>
2017-05-14 21:52:33 +0700

    add fields for user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name,
+            presence: true,
+            length: { minimum: 2, maximum: 20 }
+  validates :last_name,
+            presence: true,
+            length: { minimum: 2, maximum: 20 }
 end
